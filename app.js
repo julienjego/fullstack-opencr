f705e2d020a6d63cb59d4a4f077f441214874ee9
julienjego <jego.jj@gmail.com>
2022-11-07 17:02:12 +0100

    Add a model and simple CRUD functions

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Thing = require("./models/thing");
 
 const app = express();
 
+//Connexion à la base MongoDb
 mongoose
-    .connect(
-        "mongodb+srv://user:root@cluster0.afhwcol.mongodb.net/?retryWrites=true&w=majority",
-        { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect("mongodb://localhost:27017", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log("Connexion à MongoDB réussie !"))
     .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+//Middleware pour extraire le corps json d'une requête POST
 app.use(express.json());
 
+//Envoi des headers pour éviter les erreurs CORS
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -26,35 +30,30 @@ app.use((req, res, next) => {
     next();
 });
 
+// Méthode POST pour l'envoi de nouveaux objets
 app.post("/api/stuff", (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: "Objet créé !",
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body,
     });
+    thing
+        .save()
+        .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+        .catch((error) => res.status(400).json({ error }));
 });
 
+// Méthode GET pour récupérer tous les objets
 app.get("/api/stuff", (req, res, next) => {
-    const stuff = [
-        {
-            _id: "oeihfzeoi",
-            title: "Mon premier objet",
-            description: "Les infos de mon premier objet",
-            imageUrl:
-                "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-            price: 4900,
-            userId: "qsomihvqios",
-        },
-        {
-            _id: "oeihfzeomoihi",
-            title: "Mon deuxième objet",
-            description: "Les infos de mon deuxième objet",
-            imageUrl:
-                "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-            price: 2900,
-            userId: "qsomihvqios",
-        },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+        .then((things) => res.status(200).json(things))
+        .catch((error) => res.status(400).json({ error }));
+});
+
+// Méthode GET pour récupérer un objet
+app.get("/api/stuff/:id", (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then((thing) => res.status(200).json(thing))
+        .catch((error) => res.status(404).json({ error }));
 });
 
 module.exports = app;
